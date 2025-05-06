2fd60f29e20d0791838c6bfed8d6ab865b6ff5dc
holub25 <holub4@spsejecna.cz>
2025-05-06 20:17:44 +0200

    Jump

---
+++ src/Platform.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Platform extends JLabel {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public Platform(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.setVisible(true);
+        this.setBounds(x,y,width,height);
+        this.setBackground(Color.GREEN);
+        this.setOpaque(true);
+    }
+
+    public boolean colision(Rectangle rectangle){
+        return this.getBounds().intersects(rectangle);
+    }
+
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+}
